0c78f6396b99efc00329e89253e3bfaf68b1fc49
laiba1709 <i191720@nu.edu.pk>
2022-09-15 11:52:55 +0500

    added task1

---
+++ task1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age int
+	job string
+	salary int
+}
+
+func main() {
+var pers1 Person
+var pers2 Person
+
+// Pers1 specification
+pers1.name = "Hege"
+pers1.age = 45
+pers1.job = "Teacher"
+pers1.salary = 6000
+// Pers2 specification
+pers2.name = "Cecilie"
+pers2.age = 24
+pers2.job = "Marketing"
+pers2.salary = 4500
+
+printEmployee(pers1)
+printEmployee(pers2)
+}
+
+func printEmployee(Perso Person) {
+	fmt.Println("\nName: ", Perso.name)
+	fmt.Println("Age: ", Perso.age)
+	fmt.Println("Job: ", Perso.job)
+	fmt.Println("Salary: ", Perso.salary)
+}
